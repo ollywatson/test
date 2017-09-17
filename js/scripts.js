a95db2358c5d6789e5992e7c101fc6b10b57c693
ollywatson <oliverwcwatson@gmail.com>
2017-09-17 18:08:56 +0100

    Did my first ever git commit with Bryony as co-pilot. Awooga.

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+  $('a.scroll[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 48)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+
+
+
+  $(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('#startchange');
+   var offset = startchange.offset();
+    if (startchange.length){
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $("header").css('background-color', 'rgba(255, 255, 255, 0.9');
+       } else {
+          $('header').css('background-color', 'rgba(255, 255, 255, 0');
+       }
+   });
+    }
+});
